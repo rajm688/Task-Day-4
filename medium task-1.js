0696ee6d249045cff7b97f325d19c757f9bd8f1e
rajm688 <90443069+rajm688@users.noreply.github.com>
2021-11-24 20:03:15 +0530

    Add files via upload

---
+++ medium task-1.js
@@ -0,0 +1,47 @@
+// Question-1
+var a,b,c,d
+console.log(a,b,c,d)
+
+// question-3
+var fName 
+var lName 
+var maritalStatus 
+var country 
+var age 
+
+// question-4
+
+var fname, lname,Age,Country,Maritalstatus
+
+//questions-5
+var sTring = "string";
+var bOolean = true;
+var Undefined = undefined;
+var Null = null
+
+// question-6
+var val1 = "26"
+console.log(Number(val1))
+console.log(parseInt(val1))
+console.log(parseFloat(val1));
+
+// question-7
+//truthy values
+var bool = true
+var val2 = "string"
+var val3 = 72
+var val4 = -72
+var val5 = -Infinity
+var val6 = Infinity
+var val7 = {}
+var val8 = []
+
+//flasy values
+var val9 = ""
+var val9 = false
+var val10 = 0
+var val11 = -0
+var val12 = NaN
+var val13 =  null
+var val14 = undefined
+
---
+++ medium task-2.js
@@ -0,0 +1,65 @@
+//  Simple Programs todo for Operators
+// question-1 Square of a number
+let a = 5
+square = a**2
+console.log("square of num:"+ square )
+
+// question-2 Swapping 2 numbers
+let b =5;
+let c =10;
+let temp = b;
+b = c;
+c = temp
+console.log(b,c);
+
+// question-3 Addition of 3 numbers
+let d = 5;
+let e = 6;
+let f = 7;
+console.log(d+e+f);
+
+// question-4 Power of any number x ^ y.
+let x = 5;
+let y = 10;
+let z = x**y;
+console.log(z)
+
+// question-5 Volume Of Sphere
+let radius = 5
+console.log((4/3)*Math.PI*(radius**3))
+
+// question-6 Volume Of Prism
+///formula Volume = base * height
+let base = 10
+let height =20
+let volume = base *height
+console.log(volume)
+
+// question-7 Find area of a triangle.
+// area of triangle = height * base / 2
+let height1 = 10
+let base1 = 20
+let area = (base1 *height1)/2
+console.log(area)
+
+// question-8 asterisk pattern as shown below(No loop needed)
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+
+//question-9 all arithmetic operations.
+let num1 = 3;
+let num2 = 5;
+console.log(num1 + num2); // addition
+console.log(num1 - num2); //subtract
+console.log(num1 * num2); //multiply
+console.log(num1 ** num2); //power off
+console.log(num1 / num2); //Division
+console.log(num1 % num2); //remainder
+
+//question-10 Given their radius of a circle and find its diameter, circumference and area.
+let radius1 = 10
+console.log("area:"+((Math.PI)*(radius1**2)))
